32e8cc96257e36f2214d01ed7ad3e09dd0906af3
jamesgree90 <gree90@hotmail.com>
2016-11-18 19:13:42 -0500

    Switch to hello jenkins

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello jenkins');
 });
 
 app.listen(process.env.PORT || 5000);
